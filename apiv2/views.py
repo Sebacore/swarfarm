8fd1a64a2c66c8144e43b5a66f9ec2336aa5fcfa
porksmash <peter@porksmash.com>
2016-11-11 17:43:41 -0800

    Added default permissions for news articles

--- apiv2/views.py
+++ apiv2/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 
 from news.models import *
 from .serializers import ArticleSerializer
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
+    permission_classes = (DjangoModelPermissionsOrAnonReadOnly, )
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
