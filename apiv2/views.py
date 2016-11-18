7adab83e72929c363cdfe9ac8f93fbbf94f22762
porksmash <peter@porksmash.com>
2016-11-17 16:26:06 -0800

    Fixed user registration, permissions, etc.

--- apiv2/views.py
+++ apiv2/views.py
@@ -1,19 +1,27 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import BasePermission, AllowAny, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
+from rest_framework.permissions import BasePermission, AllowAny, IsAuthenticated, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
 
 from news.models import *
 from .serializers import *
 
 
 # Permissions
-class IsStaffOrTargetUser(BasePermission):
+class IsStaffOrUser(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_staff or view.action == 'retrieve'
+        return True
 
     def has_object_permission(self, request, view, obj):
         return request.user.is_staff or obj == request.user
+
+
+class IsStaffOrOwner(BasePermission):
+    def has_permission(self, request, view):
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff or obj.owner == request.user
 
 
 # User / Auth
@@ -22,13 +30,24 @@ class UserPagination(LimitOffsetPagination):
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
+    queryset = User.objects.none()
+    permission_classes = (IsStaffOrOwner, )
     serializer_class = UserSerializer
     pagination_class = UserPagination
 
     def get_permissions(self):
-        # Allow non-authed user to create via POST
-        return (AllowAny(), ) if self.request.method == 'POST' else (IsStaffOrTargetUser(), )
+        if self.request.method == 'POST':
+            return (AllowAny(), )
+        else:
+            return (IsStaffOrUser(), )
+
+    def get_queryset(self):
+        if self.request.user.is_staff:
+            return User.objects.all().order_by('pk')
+        elif self.request.user.is_authenticated():
+            return User.objects.filter(pk=self.request.user.pk)
+        else:
+            return User.objects.none()
 
 
 # News
